82ec0f7c24e4aa4a19221b04cbd683a315960421
velkuns <romain.inscription@hotmail.fr>
2018-02-16 13:11:42 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# psr-http-server-handler
-HTTP Server Handler - Compatible PHP 5.6 - Provides the RequestHandlerInterface of PSR-15.
+# HTTP Server Handler
+
+Provides the RequestHandlerInterface of PSR-15.
+
+Compatible PHP 5.6
+
---
+++ src/RequestHandlerInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Copyright (c) Romain Cottard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eureka\Psr\Http\Server;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * An HTTP request handler process a HTTP request and produces an HTTP response.
+ * This interface defines the methods required to use the request handler.
+ */
+interface RequestHandlerInterface
+{
+    /**
+     * Handle the request and return a response.
+     *
+     * @param  ServerRequestInterface $request
+     * @return \Eureka\Component\Response\ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request);
+}
